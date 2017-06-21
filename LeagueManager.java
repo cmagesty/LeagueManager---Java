b55bb4eb6956ffc74c96dfc3a58e8e67b41b8138
Chris Magesty <cmagesty@gmail.com>
2017-06-20 21:10:16 -0400

    Added Files
    Added initial files

---
+++ LeagueManager.java
@@ -0,0 +1,47 @@
+import com.teamtreehouse.model.Player;
+import com.teamtreehouse.model.Players;
+
+import java.io.*;
+import java.util.*;
+
+public class LeagueManager {
+
+  public static void main(String[] args) throws IOException {
+    Player[] players = Players.load();
+    System.out.printf("There are currently %d registered players.%n", players.length);
+    String choice = "";
+    Map<String, String> mMenu = new HashMap<String, String>();
+    BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+    mMenu.put("create team", "create a new team");
+    mMenu.put("add players", "add players to a team");
+    mMenu.put("remove players", "remove players from a team");
+    mMenu.put("quit", "quit program");
+      
+    
+    System.out.printf("Menu: %n");
+    for (Map.Entry<String, String> option : mMenu.entrySet()){
+      System.out.printf("%s - %s %n", option.getKey(), option.getValue());
+    }
+    
+    do {
+      System.out.printf("Choice: ");
+        choice = read.readLine();
+        switch (choice) {
+          
+          case "create team":
+          //implement
+            break;
+          case "add players":
+          //implement
+            break;
+          case "remove players":
+          //implement
+            break;
+          default :
+           System.out.printf("unknown command");
+           break;
+        }
+      } while(!choice.equals("quit"));
+  }
+}
+
